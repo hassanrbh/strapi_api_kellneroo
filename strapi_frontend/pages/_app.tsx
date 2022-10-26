91cbac91aacbc8aa30af39f814d51345a7833b44
hassantarif <hassantarif4@gmail.com>
2022-10-26 14:27:17 +0100

    adding the integration to the strapi api, configuringthe seo and image opmtimization files

--- strapi_frontend/pages/_app.tsx
+++ strapi_frontend/pages/_app.tsx
@@ -1,6 +1,63 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App, { AppContext, AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+import { createContext } from "react";
+import { fetchAPI } from "../lib/api";
+import { getStrapiMedia } from "../lib/media";
+import { RootObject } from "../types/seo";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Store Strapi Global object in context
+interface ISEOContext {
+  defaultSeo: RootObject;
+  siteName: string;
 }
+
+export const SEOContext = createContext<ISEOContext>({
+  defaultSeo: {
+    defaultSeo: {
+      id: 0,
+      metaDescription: "",
+      metaTitle: "",
+      shareImage: {
+        data: {
+          attributes: {},
+          id: 0,
+        },
+      },
+    },
+  },
+  siteName: "",
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { global } = pageProps;
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href={getStrapiMedia(global.attributes.favicon)}
+        />
+      </Head>
+      <SEOContext.Provider value={global.attributes}>
+        <Component {...pageProps} />
+      </SEOContext.Provider>
+    </>
+  );
+};
+
+MyApp.getInitialProps = async (ctx: AppContext) => {
+  const appProps = await App.getInitialProps(ctx);
+  const globalRes = await fetchAPI("/global", {
+    populate: {
+      favicon: "*",
+      defaultSeo: {
+        populate: "*",
+      },
+    },
+  });
+  return { ...appProps, pageProps: { global: globalRes.data } };
+};
+
+export default MyApp;
---
+++ strapi_frontend/types/seo.ts
@@ -0,0 +1,93 @@
+export interface Thumbnail {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+export interface Small {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+export interface Medium {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+export interface Large {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+export interface Formats {
+  thumbnail: Thumbnail;
+  small: Small;
+  medium: Medium;
+  large: Large;
+}
+
+export interface Attributes {
+  name?: string;
+  alternativeText?: string;
+  caption?: string;
+  width?: number;
+  height?: number;
+  formats?: Formats;
+  hash?: string;
+  ext?: string;
+  mime?: string;
+  size?: number;
+  url?: string;
+  previewUrl?: any;
+  provider?: string;
+  provider_metadata?: any;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface Data {
+  id: number;
+  attributes: Attributes;
+}
+
+export interface ShareImage {
+  data: Data;
+}
+
+export interface DefaultSeo {
+  id: number;
+  metaTitle: string;
+  metaDescription: string;
+  shareImage: ShareImage;
+}
+
+export interface RootObject {
+  defaultSeo: DefaultSeo;
+}
